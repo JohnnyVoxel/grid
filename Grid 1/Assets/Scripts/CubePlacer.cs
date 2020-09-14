f2cbec5ff04e85b4ff2834652b93447ff3e2e6ef
Shaun Ryan <ryan.s@kolabnow.ch>
2020-09-13 20:13:06 -0400

    Bridge hack

--- Grid 1/Assets/Scripts/CubePlacer.cs
+++ Grid 1/Assets/Scripts/CubePlacer.cs
@@ -3,6 +3,7 @@
 public class CubePlacer : MonoBehaviour
 {
     [SerializeField] private GameObject structurePrefab;
+    [SerializeField] private GameObject bridgePrefab;
     private char currentCommand = 'E';
     private Vector3 spawnPonit = new Vector3 (0.0f, -20.0f, 0.0f);
     private GameObject structure;
@@ -11,7 +12,48 @@ public class CubePlacer : MonoBehaviour
     {
         int layerMask = 1 << 8;
         int tileState = 0;
+        /////////// BRIDGE //////////////
+        if (Input.GetKeyDown(KeyCode.R) && currentCommand == 'E')
+        {
+            currentCommand = 'R';
+            structure = Instantiate(bridgePrefab, spawnPonit, Quaternion.identity);
+            structure.name = "structure";
+            structure.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (currentCommand == 'R')
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Hex selectedTile = null;
 
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                selectedTile = hit.transform.GetComponent<Hex>();
+                if(selectedTile != null)
+                {
+                    tileState = selectedTile.Structure;
+                    if(tileState == 0)
+                    {
+                        structure.transform.position = hit.transform.position;
+                        //structure.transform.parent = hit.transform;
+                        //selectedTile.Structure = 1;
+                    }
+                }
+            }
+            if (Input.GetMouseButtonDown(0) && tileState == 0)
+            {
+                structure.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.white;
+                structure.transform.parent = hit.transform;
+                //Debug.Log("Shaz");
+                selectedTile.Structure = 1;
+                currentCommand = 'E';
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Destroy(structure);
+                currentCommand = 'E';
+            }
+        }
         /////////// BUILD ///////////////
         if (Input.GetKeyDown(KeyCode.B) && currentCommand == 'E')
         {
@@ -44,7 +86,7 @@ public class CubePlacer : MonoBehaviour
             {
                 structure.transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.white;
                 structure.transform.parent = hit.transform;
-                Debug.Log("Shaz");
+                //Debug.Log("Shaz");
                 selectedTile.Structure = 1;
                 currentCommand = 'E';
             }
@@ -54,7 +96,6 @@ public class CubePlacer : MonoBehaviour
                 currentCommand = 'E';
             }
         }
-
         /////////// DESTROY ///////////////
         if (Input.GetKeyDown(KeyCode.V) && currentCommand == 'E')
         {
--- Grid 1/Assets/Scripts/Hex.cs
+++ Grid 1/Assets/Scripts/Hex.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Hex : MonoBehaviour
 {
-    public int Structure { get; set; } = 0;
+    public int Structure { get; set; } = 0; //Commit test
+    public int Edge0 { get; set; } = 0;
     public int Edge1 { get; set; } = 0;
     public int Edge2 { get; set; } = 0;
     public int Edge3 { get; set; } = 0;
     public int Edge4 { get; set; } = 0;
     public int Edge5 { get; set; } = 0;
-    public int Edge6 { get; set; } = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,34 @@ public class Hex : MonoBehaviour
 
     }
 
+    public int[] GetStatus()
+    {
+        int[] status = new int[] {this.Edge0, this.Edge1, this.Edge2, this.Edge3, this.Edge4, this.Edge5, this.Structure};
+        return status;
+    }
+
     public class Platform : Hex
     {
         public Platform()
         {
+            this.Edge0 = 1;
             this.Edge1 = 1;
+            this.Edge2 = 1;
             this.Edge3 = 1;
+            this.Edge4 = 1;
             this.Edge5 = 1;
+        }
+    }
+    public class Bridge : Hex
+    {
+        public Bridge()
+        {
+            this.Edge0 = 2;
+            this.Edge1 = 1;
+            this.Edge2 = 2;
+            this.Edge3 = 2;
+            this.Edge4 = 1;
+            this.Edge5 = 2;
         }
     }
 }
