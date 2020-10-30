6a5a94e90ab911ae679500a73c7db72b5ea89d27
Shaun Ryan <johnnyvoxel@gmail.com>
2020-10-29 20:58:14 -0400

    Aggro logic rework

--- Grid 1/Assets/Scripts/EnemyAgent.cs
+++ Grid 1/Assets/Scripts/EnemyAgent.cs
@@ -7,6 +7,8 @@ public class EnemyAgent : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Vector3 basePos;
+    private List<GameObject> aggroRangeList = new List<GameObject>();
+    private GameObject aggroAttackTarget;
     
     public Vector3 BasePos
     {
@@ -21,6 +23,22 @@ public class EnemyAgent : MonoBehaviour
         agent = GetComponent<NavMeshAgent> ();    
     }
 
+    void Update()
+    {
+        if(aggroRangeList.Count > 0)
+        {
+            MoveToLocation(aggroRangeList[0].transform.position);
+        }
+        else if(aggroAttackTarget)
+        {
+            MoveToLocation(aggroAttackTarget.transform.position);
+        }
+        else
+        {
+            MoveToLocation();
+        }
+    }
+
     public void MoveToLocation(Vector3 targetPoint)
     {
         agent.destination = targetPoint;
@@ -31,16 +49,44 @@ public class EnemyAgent : MonoBehaviour
         agent.destination = basePos;
         agent.isStopped = false;
     }
-    /*void OnTriggerEnter(Collider other) {
-        Debug.Log("Destroy!!!");
-        Destroy(gameObject);
-    }*/
-    public void AgroStart(GameObject target)
+
+    public void AddRangeAggro(GameObject target)
     {
-        MoveToLocation(target.transform.position);
+        if(!aggroRangeList.Contains(target))
+        {
+            aggroRangeList.Add(target);
+        }
     }
-    public void AgroStop(GameObject target)
+
+    public void RemoveRangeAggro(GameObject target)
     {
-        MoveToLocation();
+        if(aggroRangeList.Contains(target))
+        {
+            aggroRangeList.Remove(target);
+        }
+    }
+
+    public void AddAttackAggro(GameObject target)  //Assuming target will be the gameObject of the character, not the projectile
+    {
+        if(!aggroRangeList.Contains(target))
+        {
+            if(aggroAttackTarget == target)
+            {
+                StopCoroutine("AggroForget");   // Calling as a string causes the coroutine to restart instead of resuming... because reasons.
+                StartCoroutine("AggroForget");
+            }
+            else
+            {
+                aggroAttackTarget = target;
+                StartCoroutine("AggroForget");
+            }
+            
+        }
+    }
+
+    IEnumerator AggroForget()
+    {
+        yield return new WaitForSeconds(4);
+        aggroAttackTarget = null;
     }
 }
--- Grid 1/Assets/Scripts/TriggerAggro.cs
+++ Grid 1/Assets/Scripts/TriggerAggro.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerAgro : MonoBehaviour
+public class TriggerAggro : MonoBehaviour
 {
     public GameObject currentTarget;
     // Start is called before the first frame update
-    void OnTriggerStay(Collider other) {
+    void OnTriggerEnter(Collider other) {
         var parent = transform.parent.gameObject.GetComponent<EnemyAgent>();
         if (other.gameObject.tag == "Player")
         {
             currentTarget = other.gameObject;
-            parent.AgroStart(other.gameObject);
+            parent.AddRangeAggro(other.gameObject);
+            //parent.AddAttackAggro(other.gameObject);  // Testing attack aggro before I implemented and attack
         }
     }
     void OnTriggerExit(Collider other){
         var parent = transform.parent.gameObject.GetComponent<EnemyAgent>();
         if (other.gameObject.tag == "Player")
         {
-            parent.AgroStop(other.gameObject);
+            parent.RemoveRangeAggro(other.gameObject);
         }
     }
 }
